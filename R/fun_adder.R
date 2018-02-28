7ea35928d19988e22d5cf2e16c8b9d19668cdf73
Jonas Vinther Mikaelsen <jmikae15@student.aau.dk>
2018-02-28 15:16:49 +0100

    first commit

---
+++ R/fun_adder.R
@@ -0,0 +1,18 @@
+#' ADD two numbers
+#'
+#' This is a riveting description that describes what function does.
+#' This is a link to \code{\link{hello}}
+#'
+#' @param x a reel number
+#' @param y another number
+#'
+#' @return returns the result of addition
+#' @export
+#'
+#' @seealso
+#' \code{\link{hello}}
+#' @examples
+#' adder(1,2)
+adder <- function(x, y) {
+  return(x + y)
+}
